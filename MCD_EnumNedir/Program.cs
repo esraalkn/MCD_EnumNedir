4d99806d80ea0114c6865de3f852e9e1470bdcac
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-07 16:58:49 +0300

    Add project files.

---
+++ MCD_EnumNedir/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EnumNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            M1.id = 1;
+            M1.musteriNumara = "MSN001";
+            M1.isim = "esra";
+            M1.soyisim = "alkan";
+            M1.emailAdres = "es.alknn@gmail.com";
+
+            sanalDatabase SB = new sanalDatabase();
+            musteriReturnValue musteriKayitSonuc = SB.musteriYeniKayit(M1);
+
+            int enumInt = (int)musteriReturnValue.kayitBasarili;
+            Console.WriteLine(enumInt);
+
+            //if (musteriKayitSonuc == musteriReturnValue.kayitBasarili)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ MCD_EnumNedir/sanalDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_EnumNedir
+{
+    public class sanalDatabase
+    {
+        ArrayList sanalIDB = new ArrayList();
+
+        public musteriReturnValue musteriYeniKayit(Musteri m)
+        {
+            //Parametre kontrol
+
+            //kayıt kontrol
+
+            sanalIDB.Add(m);
+            return musteriReturnValue.kayitBasarili;
+        }
+    }
+}
